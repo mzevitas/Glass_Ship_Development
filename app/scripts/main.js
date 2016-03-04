afe44799f51238648fc7fd511bf6dcb41c96bf67
Mike Zevitas <mzevitas@sapient.com>
2016-03-04 16:27:14 -0500

    redo of the menu cover

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+$('#toggle, nav a').click(function() {
+   $('#toggle, nav a').toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
+
+// $('nav a').click(function (){
+// 	$(this).toggleClass('active');
+// 	$('#overlay').toggleClass('open');
+// });
